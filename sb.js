2d95eaa5c144226b2693b2c3ec156627e60ca466
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-06-04 06:55:24 +0530

    Add files via upload

---
+++ sb.js
@@ -0,0 +1,24 @@
+const sounds=[
+    'eagle',
+    'cartoon',
+    'kukoo',
+    'pigeon',
+    'raven',
+    'crow'
+
+]
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click",()=>{
+        document.getElementById(sound).play();
+    });
+    document.body.appendChild(btn);
+    
+
+});
+
